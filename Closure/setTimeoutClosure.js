10971e90dbce7f623cca5b2ebe4c438ec1a5e9fe
Rohit Patil <143780223+rohitpatilgithub@users.noreply.github.com>
2025-03-30 21:36:59 +0530

    Update setTimeoutClosure.js

--- Closure/setTimeoutClosure.js
+++ Closure/setTimeoutClosure.js
@@ -1,3 +1,7 @@
+// Using var try to log numbers from 1 to 5 ( after each second we should get a value )
+// 1st second we log value : 1
+// 2nd second we log value : 2....
+
 function x(){
     for(var i = 1;i<=5;i++){
         t(i);
@@ -10,3 +14,6 @@ function x(){
     console.log("This bitch will executed first")
 }
 x();
+
+// We can use let too ...
+// It is your task to find out how we can do it XD
