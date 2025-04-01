a02f570d28e0054416d34f18ecedd70bc7f4edbd
Rohit Patil <143780223+rohitpatilgithub@users.noreply.github.com>
2025-04-01 14:22:14 +0530

    Updated functionn.js

--- Function/functionn.js
+++ Function/functionn.js
@@ -21,12 +21,12 @@ const okkies = function (){
 // Difference between 'Function Statement and Function Expression'
 // is 'HOISTING'
 // 1. Statement function can be called before declaration
-// 2. Expression function cannot do it because it has to go through temporal deadzone ( use let / const / var anything ) it will make sure it says it is not a function T_T
+// 2. Expression function cannot do it because it has to go through temporal deadzone ( if we use let / const )
 
 // Named Function
 let fruit = function apple(){
     console.log("This is named function !!")
-    console.log("This named function can't be accessed at global level and I'm talking about apple() -_- ")
+    console.log("This named function can't be accessed at global level")
 }
 
 // Parameters , Arguments and Functions
@@ -41,6 +41,8 @@ function b() {
 console.log(a(b()))  // here we get different output
 console.log(a(()=>{console.log("Hemlo")}))  // here we get different output
 
-// First Class Function
-// Ability to use function as value to return a function or pass it as an argument
-// Also called as 'FIRST CLASS CITIZENS'
+// First-Class Functions
+// In JavaScript, functions are treated as first-class citizens.
+// This means we can use functions as values: 
+// - We can return a function from another function.
+// - We can pass a function as an argument to another function.
