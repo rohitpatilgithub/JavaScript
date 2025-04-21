4f4423966f5a26f2d97c080db8be6ed55dac751a
Rohit Patil <143780223+rohitpatilgithub@users.noreply.github.com>
2025-04-21 23:48:03 +0530

    Update practice.js

--- Async Js/Promises/practice.js
+++ Async Js/Promises/practice.js
@@ -1,12 +1,21 @@
+// Both blocks are consumers because we have not created a promise using constructor
+
 API = fetch('https://jsonplaceholder.typicode.com/users')
 
-createUser(API)
-    .then((response) => response.json())
-    .then((users) => console.log("Here : ",users))
+API
+    .then((res) => res.json())
+    .then((users) => users.forEach((element,index) => console.log(index+1+" : "+element.name) ))
+    .catch((err) => console.log("Error aaya",err))
 
-function createUser(URL){
-    return new Promise(function(resolve,reject){
-        URL.then(resolve)
-        .catch(reject)
-    });
-}
+
+// Why have I commented below block of promise
+// When we are fetching an API and either it is resolved / rejected
+// During this process we cannot re - trigger / re - create .then()
+
+// function callAPI(url){
+//     return url;
+// }
+
+// callAPI(API)
+//     .then((res) => res.json())
+//     .then((re) => re.forEach((element,index) => console.log( (index + 1) + " : " + element.name )))
